b0574671b6401a32c309958479404e907f216551
Siti Nasywaul <sitinasywaul77@gmail.com>
2026-04-26 16:21:08 +0700

    bagian-4-komponen UI & dark mode

--- modul-5/src/main.js
+++ modul-5/src/main.js
@@ -1,1 +1,18 @@
-import './style.css'
+import "./style.css";
+
+const btn = document.getElementById("toggleDark");
+const html = document.documentElement;
+
+if (localStorage.getItem("theme") === "dark") {
+  html.classList.add("dark");
+  btn.textContent = "Light";
+}
+
+btn.addEventListener("click", () => {
+  html.classList.toggle("dark");
+
+  const isDark = html.classList.contains("dark");
+  btn.textContent = isDark ? "Light" : "Dark";
+
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+});
